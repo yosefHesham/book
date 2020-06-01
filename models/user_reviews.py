9e8dcefc54b237b3d0e2b8c2d81b78e11cf2787f
yosefHesham <May0121463797>
2020-06-01 18:10:52 +0200

    final commit

---
+++ models/user_reviews.py
@@ -0,0 +1,9 @@
+class UserReview:
+    post = ''
+    username = ''
+    rating = ''
+
+    def __init__(self, post,username, rating):
+        self.post = post
+        self.username = username
+        self.rating = rating
